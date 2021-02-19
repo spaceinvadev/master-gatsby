569e69b31d8b95952648a994ce49141984faa445
Mauricio Paternina <spaceinvadev@gmail.com>
2021-02-18 20:22:58 -0500

    create data relationships

--- starter-files/sanity/schemas/pizza.js
+++ starter-files/sanity/schemas/pizza.js
@@ -39,5 +39,32 @@ export default {
       validation: (Rule) => Rule.min(1000),
       // TODO: Add custom input component
     },
+    {
+      name: 'toppings',
+      title: 'Toppings',
+      type: 'array',
+      of: [{ type: 'reference', to: [{ type: 'topping' }] }],
+    },
   ],
+  // Custom preview
+  preview: {
+    select: {
+      title: 'name',
+      media: 'image',
+      topping0: 'toppings.0.name',
+      topping1: 'toppings.1.name',
+      topping2: 'toppings.2.name',
+      topping3: 'toppings.3.name',
+    },
+    prepare: ({ title, media, ...toppings }) => {
+      // 1. Filter undefined toppings out
+      const topps = Object.values(toppings).filter(Boolean);
+      // 2. Return the preview objetc for the pizza
+      return {
+        title,
+        media,
+        subtitle: topps.join(', '),
+      };
+    },
+  },
 };
