605ea75ce054a2c77118813759fccc0533f3b650
Mauricio Paternina <spaceinvadev@gmail.com>
2021-03-09 21:23:01 -0500

    add styles (subgrid) to Pizza Grid

--- starter-files/gatsby/src/components/PizzasList.js
+++ starter-files/gatsby/src/components/PizzasList.js
@@ -1,25 +1,49 @@
 import React from 'react';
 import { Link } from 'gatsby';
 import GatsbyImage from 'gatsby-image';
+import styled from 'styled-components';
+
+const PizzasGridStyles = styled.div`
+  display: grid;
+  grid-template-columns: repeat(auto-fill, minmax(300px, 1fr));
+  grid-auto-rows: auto auto 500px;
+  gap: 4rem;
+`;
+
+const PizzasStyles = styled.div`
+  display: grid;
+  @supports not (grid-template-rows: subgrid) {
+    --rows: auto auto 1fr;
+  }
+  grid-template-rows: var(--rows, subgrid);
+  grid-row: span 3;
+  gap: 1rem;
+  h2,
+  p {
+    margin: 0%;
+  }
+`;
 
 function SinglePizza({ pizza }) {
   return (
-    <Link to={`pizzas/${pizza.slug.current}`}>
-      <h2>
-        <span className="mark">{pizza.name}</span>
-      </h2>
+    <PizzasStyles>
+      <Link to={`pizzas/${pizza.slug.current}`}>
+        <h2>
+          <span className="mark">{pizza.name}</span>
+        </h2>
+      </Link>
       <p>{pizza.toppings.map((topping) => topping.name).join(', ')}</p>
       <GatsbyImage fluid={pizza.image.asset.fluid} alt={pizza.name} />
-    </Link>
+    </PizzasStyles>
   );
 }
 
 export default function PizzasList({ pizzas }) {
   return (
-    <ul>
+    <PizzasGridStyles>
       {pizzas.map((pizza) => (
         <SinglePizza pizza={pizza} key={pizza.id} />
       ))}
-    </ul>
+    </PizzasGridStyles>
   );
 }
