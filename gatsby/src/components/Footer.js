3af4fc04b99286df1a0face146cb3e9b12c143d0
Mauricio Paternina <spaceinvadev@gmail.com>
2021-04-02 08:05:53 -0500

    update Footer styles

--- gatsby/src/components/Footer.js
+++ gatsby/src/components/Footer.js
@@ -1,9 +1,15 @@
 import React from 'react';
+import styled from 'styled-components';
+
+const FooterStyles = styled.footer`
+  text-align: center;
+  margin-block-start: 6.4rem;
+`;
 
 export default function Footer() {
   return (
-    <footer>
+    <FooterStyles>
       <p>&copy; Slick's Slices {new Date().getFullYear()}</p>
-    </footer>
+    </FooterStyles>
   );
 }
